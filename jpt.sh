dd753188663bea2d521d8c9cf71ea54b7bc00700
Jpt-opicial <61240021+Jpt-opicial@users.noreply.github.com>
2020-02-21 23:37:05 +0700

    Add files via upload

---
+++ jpt.sh
@@ -0,0 +1,361 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+qD='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+sh time.sh
+echo $ku"<════════════════════════════════>"
+echo $pu"Tools"$me":"$i"Terbaru"$ku" |"$pu" Versi"$me":"$i"1.0"$pu" *_*"
+echo $pu"Author"$me":"$i"THE DAXZTER"$me" &"$i" Mr.Ijun"
+echo $pu"Team"$me":"$i"JavaProgrammerTeam"
+echo $ku"<════════════════════════════════>"
+echo $me"╔════╗╔══════════════════════╗ ╔══════════╗"
+echo $me"║"$i"{"$pu"NO"$i"}"$me"║║"$i"  { "$pu"KUMPULAN TOOLS"$i" }"$me"  ║ ║"$i"{"$pu" STATUS"$i" }"$me"║"
+echo $me"╚════╝╚══════════════════════╝ ╚══════════╝"
+echo
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 01"$me" ║║"$pu" Background Termux"$i" V1"$me" ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 02"$me" ║║"$pu" Background Termux"$i" V2"$me" ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 03"$me" ║║"$pu" Virus Maker"$i" New"$me"      ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 04"$me" ║║"$pu" Hack Fb"$i" V1"$me"           ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 05"$me" ║║"$pu" Hack Fb"$i" V2"$me"           ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 06"$me" ║║"$pu" Tools RED HAWK"$me"       ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 07"$me" ║║"$pu" DDOS Website"$me"         ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 08"$me" ║║"$pu" Website J-P-T"$me"        ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 09"$me" ║║"$pu" Hack-Camera"$me"          ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 10"$me" ║║"$pu" Anggota JPT Official"$me" ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 11"$me" ║║"$pu" Torjan Attack"$me"        ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 12"$me" ║║"$pu" Spam-Telphone"$me"        ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 13"$me" ║║"$pu" Tebas-Index"$me"          ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 14"$me" ║║"$pu" Join JPT Publik"$i" Wa"$me"   ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 15"$me" ║║"$pu" Hack-Pulsa"$i" Free"$me"      ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 16"$me" ║║"$pu" Game-Termux"$me"          ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 17"$me" ║║"$pu" Apk-Hacking-Android"$me"  ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 18"$me" ║║"$pu" Tools Sqlmap"$me"         ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 19"$me" ║║"$pu" Jasa Admin"$i" No Free"$me"   ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 20"$me" ║║"$pu" Admin-Finder"$me"         ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 21"$me" ║║"$pu" Encrypt Python"$me"       ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 22"$me" ║║"$pu" Perkiraan Cuaca"$me"      ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 23"$me" ║║"$pu" Asma'ul Khusna"$i" New"$me"   ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 24"$me" ║║"$pu" Chat-Admin"$i" Wa"$me"        ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 25"$me" ║║"$pu" Pish All Project"$me"     ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 26"$me" ║║"$pu" Join JPT Official"$me"    ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 27"$me" ║║"$pu" Artimatika"$me"           ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 28"$me" ║║"$pu" Install Metasploit"$me"   ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 29"$me" ║║"$pu" Pelacak Orang"$me"        ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"
+echo $me"╔════╗╔══════════════════════╗    ╔════╗"
+echo $me"║"$i" 30"$me" ║║"$pur" Exit / Keluar"$me"        ║    ║"$i" ON"$me" ║"
+echo $me"╚════╝╚══════════════════════╝    ╚════╝"$pu""
+echo
+read -p "PILIH NOMOR BANGSAT :" vil
+if [ $vil = '01' ]
+then
+clear
+git clone https://github.com/TU4ND3VIL/BackgroundTermux
+cd BackgroundTermux
+python2 run.py
+fi
+
+if [ $vil = '02' ]
+then
+clear
+git clone https://github.com/TU4ND3VIL/BackgroundTermuxV2
+cd BackgroundTermuxV2
+python2 v22.py
+fi
+
+if [ $vil = '03' ]
+then
+clear
+git clone https://github.com/DrCyber01/Virus-Maker
+cd Virus-Maker
+sh Virus.sh
+fi
+
+if [ $vil = '04' ]
+then
+clear
+pip2 install --upgrade pip
+git clone https://github.com/TU4ND3VIL/DarkFB
+cd DarkFB
+python2 fb.py
+fi
+
+if [ $vil = '05' ]
+then
+clear
+git clone https://github.com/pashayogi/Fb-hack
+cd Fb-hack
+python2 FB-HACK.py
+fi
+
+if [ $vil = '06' ]
+then
+clear
+pkg install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $vil = '07' ]
+then
+clear
+python2 A1.py
+fi
+
+
+if [ $vil = '08' ]
+then
+clear
+echo $ku"Anda Akan Di Arahkan Ke"
+sleep 3
+echo $ku"Website JavaProgrammerTeam"
+sleep 3
+xdg-open http://javaprogrammerteam.eu5.org/
+fi
+
+
+if [ $vil = '09' ]
+then
+clear
+git clone https://github.com/TU4ND3VIL/SadapCamera
+cd SadapCamera
+bash tu4nd3vil.sh
+fi
+
+if [ $vil = '10' ]
+then
+clear
+sh A2.sh
+fi
+
+if [ $vil = '11' ]
+then
+clear
+git clone https://github.com/TU4ND3VIL/TrojansAttack
+cd TrojansAttack
+python2 trojans.py
+fi
+
+if [ $vil = '12' ]
+then
+clear
+git clone https://github.com/storiku/Call
+cd Call
+php spamcall.php
+fi
+
+if [ $vil = '13' ]
+then
+clear
+git clone https://github.com/BlackCyber091/TEBAS-INDEX
+cd TEBAS-INDEX
+sh TEBAS.sh
+fi
+
+if [ $vil = '14' ]
+then
+clear
+xdg-open https://chat.whatsapp.com/KDDq7SkQite1ULcUi0TXym
+fi
+
+if [ $vil = '15' ]
+then
+clear
+git clone https://github.com/TU4ND3VIL/PulsaGratis
+cd PulsaGratis
+sh gratis.sh
+fi
+
+if [ $vil = '16' ]
+then
+clear
+git clone https://github.com/TU4ND3VIL/Game-Termux
+cd Game-Termux
+sh Game.sh
+fi
+
+if [ $vil = '17' ]
+then
+clear
+git clone https://github.com/DrCyber099/ToolsG
+cd ToolsG
+sh keren.sh
+fi
+
+if [ $vil = '18' ]
+then
+clear
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $vil = '19' ]
+then
+clear
+sh js.sh
+fi
+
+if [ $vil = '20' ]
+then
+clear
+git clone https://github.com/jpt-official/admin-finder
+cd admin-finder
+python2 adminfind.py
+fi
+
+if [ $vil = '21' ]
+then
+clear
+git clone https://github.com/TU4ND3VIL/PythonEncript
+cd PythonEncript
+python2 enc.py
+fi
+
+
+if [ $vil = '22' ]
+then
+clear
+git clone https://github.com/TU4ND3VIL/PerkiraanCauca
+cd PerkiraanCuaca
+sh cuaca.sh
+fi
+
+if [ $vil = '23' ]
+then
+clear
+git clone https://github.com/TU4ND3VIL/Asmaul-Khusna
+cd Asmaul-Khusna
+sh 99.sh
+fi
+
+if [ $vil = '24' ]
+then
+clear
+sh js.sh
+fi
+
+if [ $vil = '25' ]
+then
+clear
+git clone https://github.com/reyspeed/phish-all
+cd phish-all
+bash croot.sh
+fi
+
+if [ $vil = '26' ]
+then
+clear
+echo $pu"Jika anda masuk harus siap tes"
+sleep 4
+echo $pu"No Dual"
+sleep 5
+xdg-open http://join.javaprogrammerteam.dx.am/
+fi
+
+if [ $vil = '27' ]
+then
+clear
+sh A3.sh
+fi
+
+
+if [ $vil = '28' ]
+then
+clear
+pkg install git
+git clone https://github.com/Hax4us/Metasploit_termux
+cd Metasploit_termux
+chmod +x metasploit.sh
+./metasploit.sh
+fi
+
+
+if [ $vil = '29' ]
+then
+clear
+git clone https://github.com/reyspeed/pelacak-maling
+cd pelacak-maling
+bash locator.sh
+fi
+
+if [ $vil = '30' ]
+then
+clear
+echo $pur"Thanks Udh Berkunjung Di Tools JPT"
+sleep 3
+echo $pur"Balik lagi ya...."
+exit
+fi
